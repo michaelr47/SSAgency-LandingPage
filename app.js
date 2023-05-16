ac1296930628025a80b5db2ae1caa623dc2c830b
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-05-15 21:51:29 -0700

    diplaying hamburger menu at tablet pov and lower

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+const hamburgerMenu = document.getElementById('hamburgerMenu');
 
 const innerWidth = () => {
-    let width = window.innerWidth;
+    let widthOfScreen = window.innerWidth;
+    if (widthOfScreen <= 768) {
+        hamburgerMenu.style.display = 'block';
+    } else {
+        hamburgerMenu.style.display = 'none';
+    }
 }
 
 window.addEventListener('resize', innerWidth);
