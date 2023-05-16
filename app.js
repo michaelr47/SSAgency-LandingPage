58c24955744a50fe71ec291b4b177a393149c291
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2023-05-15 22:07:08 -0700

    remove br tags on text

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const hamburgerMenu = document.getElementById('hamburgerMenu');
-
+const breakTags = document.querySelectorAll('.br');
 const innerWidth = () => {
     let widthOfScreen = window.innerWidth;
     if (widthOfScreen <= 768) {
         hamburgerMenu.style.display = 'block';
     } else {
         hamburgerMenu.style.display = 'none';
+    }
+
+    if (widthOfScreen <= 915) {
+        for (let i = 0; i < breakTags.length; i++) {
+           breakTags[i].remove(); 
+        } 
     }
 }
 
